55966e23d8f1184518f71b99bab8301bee0f416a
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2026-02-14 00:48:33 +0400

    feat: add get pagination range function

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,8 +1,24 @@
-function Pagination() {
+import { LIMIT, TOTAL_PAGES } from '../../constants/constants';
+import { useEffect, useLayoutEffect, useState } from 'react';
+
+import getPaginationRange from '../../helpers/getPaginationRange';
+import styles from './Pagination.module.css';
+
+function Pagination({}) {
+  const range = getPaginationRange(1, 24);
+
   return (
     <div>
       <button>{'<'}</button>
-      <div>1</div>
+      <div>
+        {range.map((pageNumber) => {
+          if (pageNumber === '...') {
+            return <span>...</span>;
+          } else {
+            return <button key={pageNumber}>{pageNumber}</button>;
+          }
+        })}
+      </div>
       <button>{'>'}</button>
     </div>
   );
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const LIMIT = 10;
+export const TOTAL_PAGES = 165;
