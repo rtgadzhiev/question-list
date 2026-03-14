063e1a1b461b22732e8206f064d90b28be9751b1
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2026-03-14 19:50:29 +0400

    feat: add searh params in the pagination component

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -17,7 +17,7 @@ function Questions() {
           <FiltersButton onClick={toggleFilters} />
         </header>
         <QuestionsAccordion />
-        {/* <QuestionsPagination /> */}
+        <QuestionsPagination />
       </Card>
     </section>
   );
