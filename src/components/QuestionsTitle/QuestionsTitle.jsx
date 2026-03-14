1cb11d2590aa04c9ecb7bdd7f3a24ea81c4d568f
Ruslan Gadzhiev <rtgadzhiev@gmail.com>
2026-03-14 22:21:59 +0400

    feat: add skeleton for questions title

--- src/components/QuestionsTitle/QuestionsTitle.jsx
+++ src/components/QuestionsTitle/QuestionsTitle.jsx
@@ -2,16 +2,22 @@ import { getSpecialization } from '../../api/apiQuestions';
 import styles from './QuestionsTitle.module.css';
 import useFetch from '../../helpers/hooks/useFetch';
 import { useSearchParams } from 'react-router';
+import Skeleton from '../ui/Skeleton/Skeleton';
 
 function QuestionsTitle() {
   const [searchParams] = useSearchParams();
   const specializationId = searchParams.get('specializationId');
 
-  const [data] = useFetch(getSpecialization, specializationId);
+  const [data, isLoading] = useFetch(getSpecialization, specializationId);
 
   return (
     <h1 className={styles.title}>
-      Вопросы{data?.title ? ` ${data?.title}` : ''}
+      Вопросы
+      {isLoading ? (
+        <Skeleton className={styles.skeleton} />
+      ) : (
+        data?.title && ` ${data?.title}`
+      )}
     </h1>
   );
 }
